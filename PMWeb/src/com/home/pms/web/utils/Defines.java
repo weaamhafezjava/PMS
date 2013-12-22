0a2b4511f7264fabbb2d920c9265f49baeb79e02
Weaam <Weaam@192.168.0.101>
2013-12-22 16:10:43 +0400

    New added

---
+++ PMWeb/src/com/home/pms/web/utils/Defines.java
@@ -0,0 +1,10 @@
+package com.home.pms.web.utils;
+
+public class Defines {
+
+	// spring conf used for saving all beans needed to debug the methods time performance
+	public static final String DEBUG_LOG_CONF_FILEPATH = "debugBeansContext.xml";
+	
+	// beans names in debugBeansContext.xml
+	public static final String PMS_LOGGER_BEAN = "PmsLogger";
+}
