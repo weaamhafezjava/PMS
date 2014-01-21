391b55e7dfbf1f0d9d3f66cb318730aa1070a017
Weaam <Weaam@192.168.0.103>
2014-01-21 20:29:39 +0400

    Dao handling

--- PMSCore/src/com/whsolutions/core/dao/GenericDao.java
+++ PMSCore/src/com/whsolutions/core/dao/GenericDao.java
@@ -14,4 +14,6 @@ public class GenericDao {
 
 	@Autowired
 	public SessionFactory sessionFactory;
+	
+	
 }
--- PMSCore/src/com/whsolutions/core/service/impl/UsersServiceImpl.java
+++ PMSCore/src/com/whsolutions/core/service/impl/UsersServiceImpl.java
@@ -1,16 +1,20 @@
 package com.whsolutions.core.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.whsolutions.core.dao.PmsUsersDao;
 import com.whsolutions.core.service.UsersService;
 
 public class UsersServiceImpl implements UsersService {
 
+	@Autowired
+	public PmsUsersDao userDao;
+	
 	@Transactional
 	@Override
 	public void addUser() {
-		// TODO Auto-generated method stub
-		
+		userDao.findById(1);
 	}
 
 }
